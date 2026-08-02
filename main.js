3ded286ae470170bec741ca0a8195d4826bc9f20
anushreer25 <112209220+anushreer25@users.noreply.github.com>
2026-08-01 22:31:06 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 console.log("D3 loaded:", d3);
+
+d3.csv("coffee.csv").then(function(data) {
+  console.log("Data loaded:", data);
+  console.log("Number of rows:", data.length);
+  console.log("First row:", data[0]);
+});
